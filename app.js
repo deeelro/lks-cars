3a33f2a3677b710a6ff671db0051c1d454884feb
ivan <ivan.rio@educa.madrid.org>
2025-05-09 11:56:55 +0200

    Login, logout, axios, cors, vistas de alertas, error al obtener un vehiculo que no existe, perfil de usuario, actualizacion de datos y contraseña de usuario

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const globalErrorHandler = require('./controllers/errorController');
 const carRouter = require('./routes/carRoutes');
 const userRouter = require('./routes/userRoutes');
 const viewRouter = require('./routes/viewRoutes');
 // const errorMiddleware = require('./utils/errorMiddleware');
-const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -14,11 +16,14 @@ app.set('view engine', 'pug'); // Configuración del motor de plantillas
 app.set('views', path.join(__dirname, 'views')); // de aqui se cargan las vistas
 app.use(express.static(path.join(__dirname, 'public'))); // de aqui se cargan archivos estáticos
 
-
-app.use(cors()); // Middleware para permitir peticiones de otros dominios
+app.use(cors({  // Middleware para permitir peticiones de otros dominios
+    origin: 'http://localhost:3000',
+    credentials: true, // Permitir cookies y credenciales
+})); 
 
 // MIDDLEWARES
 app.use(express.json()); // Middleware para peticiones POST y PUT
+app.use(cookieParser()); // Middleware para parsear cookies
 
 // RUTAS
 app.use('/', viewRouter);
---
+++ public/js/updateSettings.js
@@ -0,0 +1,54 @@
+import { showAlert } from "./alerts.js";
+
+// Actualizar nombre y email o contraseña
+const updateSettings = async (data, type) => {
+    try {
+        const url = type === 'password' // Si el tipo es password
+            ? 'http://localhost:3000/api/v1/users/updateMyPassword' // Actualiza la contraseña
+            : 'http://localhost:3000/api/v1/users/updateMe'; // Actualiza el nombre y email
+
+        const res = await axios({
+            method: 'PATCH', // Método PATCH para actualizar
+            url, // URL de la API
+            data // Datos a enviar
+        })
+
+        if (res.data.status === 'success') { // Si la respuesta es exitosa
+            showAlert('success', 'Datos actualizados correctamente'); // Muestra un mensaje de éxito
+        }
+    } catch (err) {
+        showAlert('error', err.response.data.message); // Muestra un mensaje de error
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const userDataForm = document.querySelector('.form-user-data'); // Selecciona el formulario de datos del usuario
+    const userPasswordForm = document.querySelector('.form-user-password'); // Selecciona el formulario de contraseña
+    if (userDataForm) {
+        userDataForm.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const name = document.querySelector('#name').value; // Obtiene el nombre
+            const email = document.querySelector('#email').value; // Obtiene el nombre
+            updateSettings({ name, email}, 'data'); // Llama a la función para actualizar los datos
+        })
+    }
+    if (userPasswordForm) {
+        userPasswordForm.addEventListener('submit', async (e) => {
+            e.preventDefault(); // Evita el comportamiento por defecto del formulario
+            document.querySelector('.btn--save-password').textContent = 'Actualizando...'; // Cambia el texto del botón a "Actualizando..."
+            const passwordCurrent = document.querySelector('#password-current').value; // Obtiene la contraseña actual
+            const password = document.querySelector('#password').value; // Obtiene la nueva contraseña
+            const passwordConfirm = document.querySelector('#password-confirm').value; // Obtiene la confirmación de la nueva contraseña
+            
+            
+            await updateSettings({ passwordCurrent, password, passwordConfirm }, 'password'); // Llama a la función para actualizar la contraseña
+
+            // Elimino los valores de los campos
+            document.querySelector('#password-current').value = ''; 
+            document.querySelector('#password').value = ''; 
+            document.querySelector('#password-confirm').value = ''; 
+            document.querySelector('.btn--save-password').textContent = 'Cambiar contraseña'; // Cambia el texto del botón a "Actualizando..."
+
+        })
+    }
+});
